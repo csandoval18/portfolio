1faea28e58282250c5a5405e2e7cc5722ff66f52
Christian Sandoval <christiansandoval1018@outlook.com>
2024-06-19 15:40:16 -0500

    update

--- src/layout/Home.tsx
+++ src/layout/Home.tsx
@@ -4,32 +4,34 @@ interface HomeProps {}
 
 const Home: React.FC<HomeProps> = () => {
   return (
-    <section className="Home container">
-      hello world
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem corporis
-        molestias eligendi, nam quasi ullam, consectetur perspiciatis adipisci
-        accusamus odit nesciunt architecto distinctio ad ipsam neque esse
-        maiores pariatur repellat.
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem corporis
-        molestias eligendi, nam quasi ullam, consectetur perspiciatis adipisci
-        accusamus odit nesciunt architecto distinctio ad ipsam neque esse
-        maiores pariatur repellat.
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem corporis
-        molestias eligendi, nam quasi ullam, consectetur perspiciatis adipisci
-        accusamus odit nesciunt architecto distinctio ad ipsam neque esse
-        maiores pariatur repellat.
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem corporis
-        molestias eligendi, nam quasi ullam, consectetur perspiciatis adipisci
-        accusamus odit nesciunt architecto distinctio ad ipsam neque esse
-        maiores pariatur repellat.
-      </p>
+    <section className="Home">
+      <section className="Hero">Hero Section</section>
+      <div className="container">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem
+          corporis molestias eligendi, nam quasi ullam, consectetur perspiciatis
+          adipisci accusamus odit nesciunt architecto distinctio ad ipsam neque
+          esse maiores pariatur repellat.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem
+          corporis molestias eligendi, nam quasi ullam, consectetur perspiciatis
+          adipisci accusamus odit nesciunt architecto distinctio ad ipsam neque
+          esse maiores pariatur repellat.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem
+          corporis molestias eligendi, nam quasi ullam, consectetur perspiciatis
+          adipisci accusamus odit nesciunt architecto distinctio ad ipsam neque
+          esse maiores pariatur repellat.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem
+          corporis molestias eligendi, nam quasi ullam, consectetur perspiciatis
+          adipisci accusamus odit nesciunt architecto distinctio ad ipsam neque
+          esse maiores pariatur repellat.
+        </p>
+      </div>
     </section>
   )
 }
