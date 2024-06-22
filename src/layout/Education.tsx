d70175865559ac4f9d2e0daabb29502691e0e5d4
Christian Sandoval <christiansandoval1018@outlook.com>
2024-06-22 16:06:36 -0500

    fixed styles of education list

--- src/layout/Education.tsx
+++ src/layout/Education.tsx
@@ -12,26 +12,30 @@ const Education: React.FC<EducationProps> = () => {
         <h2>Education</h2>
       </div>
       <div className="education-content container">
-        <h3>
-          <LiaUniversitySolid />
-          University of Wisconsin - <span>Whitewater</span>
-        </h3>
+        <div className="university-header">
+          <h3>
+            <LiaUniversitySolid />
+            University of Wisconsin - <span>Whitewater</span>
+          </h3>
+        </div>
         <p>Major: Bachelors of Science - Computer Science</p>
         <p>GPA: 3.4</p>
 
-        <h4>Relevant Courses</h4>
-        <ul>
-          <li>Data Structures</li>
-          <li> Theory of Algorithms</li>
-          <li> Server-Side Scripting</li>
-          <li>Advanced Web App Development</li>
-          <li>Theory of Computation</li>
-          <li>Visual Design for Digital Media</li>
-          <li>JavaScript/DHTML</li>
-          <li>Software Engineering</li>
-          <li>Web Server and Unix Administration</li>
-          <li>Computer Organization/ System</li>
-        </ul>
+        <div className="courses">
+          <h4>Relevant Courses</h4>
+          <ul>
+            <li>Data Structures</li>
+            <li> Theory of Algorithms</li>
+            <li> Server-Side Scripting</li>
+            <li>Advanced Web App Development</li>
+            <li>Theory of Computation</li>
+            <li>Visual Design for Digital Media</li>
+            <li>JavaScript/DHTML</li>
+            <li>Software Engineering</li>
+            <li>Web Server and Unix Administration</li>
+            <li>Computer Organization/ System</li>
+          </ul>
+        </div>
       </div>
     </section>
   )
