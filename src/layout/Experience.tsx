66e8e4f3626bf43a2b7cd9e7f5589bd38c79f173
Christian Sandoval <christiansandoval1018@outlook.com>
2024-06-23 23:55:55 -0500

    progressed with styling

--- src/layout/Experience.tsx
+++ src/layout/Experience.tsx
@@ -10,6 +10,36 @@ const Experience: React.FC<ExperienceProps> = () => {
         <CgToolbox />
         <h2>Experience</h2>
       </div>
+      <div className="experience-content container">
+        <h4>Front End Developer | Foremost Media:</h4>
+        <span>Sep 2022 – June 2023</span>
+        <ul>
+          <li>
+            Implemented client web user interfaces with attention to detail from
+            website design templates.
+          </li>
+          <li>
+            Updated and maintained client website assets, content, plugins,
+            extensions, and libraries through CMS software such as NopCommerce,
+            DNN, Wordpress, etc.
+          </li>{" "}
+          <li>
+            Performed SEO maintenance on client websites, improving and
+            generating structured data for website content. Modernized
+            environment and UI of legacy and outdated client websites.
+          </li>{" "}
+          <li>
+            Implemented dynamic web applications based on client requirements.
+          </li>
+        </ul>
+        <h4> Student Developer | University of Wisconsin - Whitewater</h4>
+        <span>Apr 2021 – May 2022</span> Developed and maintained web
+        applications using Laravel, HTML, jQuery, and MySQL. The applications
+        were for use of residence hall front desk workers, students, and staff.
+        Developed and maintained University domain pages for the housing
+        department using Ingeniux CMS. Troubleshoot problems and aiding students
+        with personal account issues in the campus system.
+      </div>
     </section>
   )
 }
