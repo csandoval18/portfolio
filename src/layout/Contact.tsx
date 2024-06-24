e0f022aa0eb6a991b2ff9e784efdd72927a53737
Christian Sandoval <christiansandoval1018@outlook.com>
2024-06-23 19:03:23 -0500

    update

--- src/layout/Contact.tsx
+++ src/layout/Contact.tsx
@@ -1,116 +1,50 @@
-import React, { useState } from "react"
-import { GrContactInfo } from "react-icons/gr"
-// import { GrContactInfo } from "react-icons/gr"
+import { useState } from "react"
+import ContactSVG from "../assets/contact.svg" // Make sure to have a contact.svg file in your project
 
-interface ContactProps {}
-
-const Contact: React.FC<ContactProps> = () => {
-  // let fadeInScreenHandler = (screen: any) => {
-  //   if (screen.fadeInScreen !== props.id) return
-  //   Animations.animations.fadeInScreen(props.id)
-  // }
-
-  // ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler)
-
-  const [name, setName] = useState<string>("")
-  const [email, setEmail] = useState<string>("")
-  const [message, setMessage] = useState<string>("")
-  // const [banner, setBanner] = useState<string>("")
-  // const [bool, setBool] = useState<Boolean>(false)
+const Contact = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
 
   return (
-    <section className="Contact">
-      <div className="category-header">
-        <GrContactInfo />
-        <h2>Contact</h2>
-      </div>
-      <div className="main-container fade-in">
-        {/* // id={props.id || ""}> */}
-        {/* <ScreenHeading subHeading={"Send me a message"} title={"Contact Me"} /> */}
-        <div className="central-form">
-          <div className="row">
-            <h2 className="title">
-              {/* <TypeAnimation
-              cursor={true}
-              repeat={1}
-              sequence={["Send your message here:", 2000]}
-            /> */}
-            </h2>
+    <section className="main-container">
+      <div className="central-form">
+        <div className="svg-container">{ContactSVG}</div>
+        <h2>Contact Us</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
           </div>
-          <div className="back-form">
-            <div className="img-back">
-              {/* <img src={contactMeBG} alt="unable to load" /> */}
-            </div>
-            <form
-            // onSubmit={async (e) => {
-            //   e.preventDefault()
-            //   try {
-            //     let data = {
-            //       name,
-            //       email,
-            //       message,
-            //     }
-            //     setBool(true)
-            //     const res = await axios.post(
-            //       "http://localhost:5000/contact",
-            //       data
-            //     )
-            //     if (
-            //       name.length === 0 ||
-            //       email.length === 0 ||
-            //       message.length === 0
-            //     ) {
-            //       setBanner(res.data.msg)
-            //       setBool(false)
-            //     } else if (res.status === 200) {
-            //       setBanner(res.data.msg)
-            //       setBool(false)
-
-            //       //reset inputs vars
-            //       setName("")
-            //       setEmail("")
-            //       setMessage("")
-            //     }
-            //   } catch (error) {
-            //     console.log("Error: " + error)
-            //   }
-            // }}
-            >
-              {/* <p>{banner}</p> */}
-              <label htmlFor="name">Name</label>
-              <input
-                type="text"
-                onChange={(e) => setName(e.target.value)}
-                value={name}
-              />
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-              />
-              <label htmlFor="message">Message</label>
-              <textarea
-                // @ts-ignore
-                type="text"
-                onChange={(e) => setMessage(e.target.value)}
-                value={message}
-              />
-              <div className="btn-wrapper">
-                <button className="submit-btn" type="submit">
-                  Send
-                </button>
-                {/* {bool ? (
-                  <b classname="load">
-                    {/* <img src={load} alt="unable to load" /> */}
-                {/* </b>
-                ) : (
-                  ""
-                )} */}
-              </div>
-            </form>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
           </div>
-        </div>
+          <div className="form-group">
+            <label htmlFor="message">Message</label>
+            <textarea
+              id="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit" className="submit-btn">
+            Send
+          </button>
+        </form>
       </div>
     </section>
   )
