152b0d40bab27bac560a17f355e57987044363b9
Christian Sandoval <christiansandoval1018@outlook.com>
2025-12-06 00:31:19 -0600

    swapped svg icons to webps in carousel

---
+++ src/components/Carousel (SVGs).tsx
@@ -0,0 +1,53 @@
+import { BiLogoPostgresql } from "react-icons/bi"
+import {
+  FaAws,
+  FaDocker,
+  FaHtml5,
+  FaNode,
+  FaPython,
+  FaReact
+} from "react-icons/fa"
+import {
+  SiCplusplus,
+  SiJavascript,
+  SiRedis,
+  SiSass
+} from "react-icons/si"
+import LazyIcon from "./LazyIcon"
+
+interface CarouselProps {}
+
+
+const Carousel: React.FC<CarouselProps> = () => {
+  const icons = <>
+    <LazyIcon><FaPython className="icon" /></LazyIcon>
+    <LazyIcon><SiCplusplus className="icon" /></LazyIcon>
+    <LazyIcon><BiLogoPostgresql className="icon" /></LazyIcon>
+    <LazyIcon><SiRedis className="icon" /></LazyIcon>
+    <LazyIcon><FaNode className="icon" /></LazyIcon>
+    <LazyIcon><FaHtml5 className="icon" /></LazyIcon>
+    <LazyIcon><SiSass className="icon" /></LazyIcon>
+    <LazyIcon><SiJavascript className="icon" /></LazyIcon>
+    <LazyIcon><FaReact className="icon" /></LazyIcon>
+    <LazyIcon><FaAws className="icon" /></LazyIcon>
+    <LazyIcon><FaDocker className="icon" /></LazyIcon>
+    </>
+
+  return (
+    <div className="Carousel">
+      <div className="logo-slider" 
+      >
+        <div className="logos-slide"
+        >
+        {icons}
+        </div>
+        <div className="logos-slide"
+        >
+        {icons}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Carousel
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,38 +1,34 @@
-import { BiLogoPostgresql } from "react-icons/bi"
-import {
-  FaAws,
-  FaDocker,
-  FaHtml5,
-  FaNode,
-  FaPython,
-  FaReact
-} from "react-icons/fa"
-import {
-  SiCplusplus,
-  SiJavascript,
-  SiRedis,
-  SiSass
-} from "react-icons/si"
-import LazyIcon from "./LazyIcon"
-
 interface CarouselProps {}
+import cppIcon from '../assets/carousel-icons/cpp-icon.webp'
+import dockerIcon from '../assets/carousel-icons/docker-icon.webp'
+import golangIcon from '../assets/carousel-icons/golang-icon.webp'
+import htmlIcon from '../assets/carousel-icons/html-icon.webp'
+import jsIcon from '../assets/carousel-icons/js-icon.webp'
+import tsIcon from '../assets/carousel-icons/ts-icon.webp'
+import mongodbIcon from '../assets/carousel-icons/mongodb-icon.webp'
+import postgresIcon from '../assets/carousel-icons/postgres-icon.webp'
+import pythonIcon from '../assets/carousel-icons/python-icon.webp'
+import reactIcon from '../assets/carousel-icons/react-icon.webp'
+import redisIcon from '../assets/carousel-icons/redis-icon.webp'
+import sassIcon from '../assets/carousel-icons/sass-icon.webp'
+import LazyIcon from './LazyIcon'
+
 
 
 const Carousel: React.FC<CarouselProps> = () => {
   const icons = <>
-    <LazyIcon><FaPython className="icon" /></LazyIcon>
-    <LazyIcon><SiCplusplus className="icon" /></LazyIcon>
-    <LazyIcon><BiLogoPostgresql className="icon" /></LazyIcon>
-    <LazyIcon><SiRedis className="icon" /></LazyIcon>
-    <LazyIcon><FaNode className="icon" /></LazyIcon>
-    <LazyIcon><FaHtml5 className="icon" /></LazyIcon>
-    <LazyIcon><SiSass className="icon" /></LazyIcon>
-    <LazyIcon><SiJavascript className="icon" /></LazyIcon>
-    <LazyIcon><FaReact className="icon" /></LazyIcon>
-    <LazyIcon><FaAws className="icon" /></LazyIcon>
-    <LazyIcon><FaDocker className="icon" /></LazyIcon>
+      <LazyIcon><img className="icon" src={cppIcon} alt="cpp-icon" /></LazyIcon>
+      <LazyIcon><img className="icon" src={jsIcon} alt="js-icon" /></LazyIcon>
+      <LazyIcon><img className="icon" src={tsIcon} alt="ts-icon" /></LazyIcon>
+      <LazyIcon><img className="icon" src={pythonIcon} alt="python-icon" /></LazyIcon>
+      <LazyIcon><img className="icon" src={golangIcon} alt="golang-icon" /></LazyIcon>
+      <LazyIcon><img className="icon" src={reactIcon} alt="react-icon" /></LazyIcon>
+      <LazyIcon><img className="icon" src={sassIcon} alt="sass-icon" /></LazyIcon>
+      <LazyIcon><img className="icon" src={mongodbIcon} alt="mongodb-icon" /></LazyIcon>
+      <LazyIcon><img className="icon" src={postgresIcon} alt="postgres-icon" /></LazyIcon>
+      <LazyIcon><img className="icon" src={redisIcon} alt="redis-icon" /></LazyIcon>
+      <LazyIcon><img className="icon" src={dockerIcon} alt="docker-icon" /></LazyIcon>
     </>
-
   return (
     <div className="Carousel">
       <div className="logo-slider" 
